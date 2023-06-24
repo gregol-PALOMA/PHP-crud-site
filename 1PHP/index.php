e6dab804bab87106782a04af7db1c679af36c98f
gregol_PALOMA <106928802+gregol-PALOMA@users.noreply.github.com>
2023-06-23 20:24:41 -0400

    Update index.php

--- 1PHP/index.php
+++ 1PHP/index.php
@@ -3,11 +3,4 @@
 //TODO: Redireciona para a pagina de login
 header('Location: ./controll/loginController.php');
 
-// TRABALHO REALIZADO PELA ALUNA PALOMA GREGOL DA TURMA 2210 - 5° Semestre informatica, vespertino
-
-
-    //! verificar se md5 e cookies estão funcinando
-
-    //! implementar filtro
-
-    //! verificar se problema na classe materias.class.php persiste mesmo em outras maquinas
+// TRABALHO REALIZADO POR PALOMA GREGOL
