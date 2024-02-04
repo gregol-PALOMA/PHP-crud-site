6b069b1d499eea8d1866e84008a2fb91e855389c
gregol-PALOMA <gregolalvarenga@gmail.com>
2024-02-04 13:00:22 -0400

    :construction: feat: Adicionando pastas e paginas.

--- Model/admModel.php
+++ Model/admModel.php
@@ -1,1 +1,16 @@
 <?
+    require_once('./../database.php');
+
+    class adm{
+        private $name;
+        private $email;
+        private $password;
+
+        public function __contruct($name, $email, $password){
+            $this->name = $name;
+            $this->email = $email;
+            $this->password = $password;
+        }
+
+        
+    }
