a735afe1a01bc829e116aa6f1d9b294e5c1ba864
gregol-PALOMA <gregolalvarenga@gmail.com>
2024-02-10 17:20:22 -0400

    :construction: Começando o front do login

---
+++ SESAMO/View/loginView.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <style>
+        body {
+            background-image: url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fak6.picdn.net%2Fshutterstock%2Fvideos%2F31206496%2Fthumb%2F1.jpg&f=1&nofb=1&ipt=a82709eb36c008834339ed02265f1bf1ebed92c2d8d3837c9359a35ea1910065&ipo=images);
+            background-size: 8%;
+            overflow: hidden;
+            background-position: center;
+            background-repeat: repeat;
+            background-color: #fff;
+            transition: all 200ms linear;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="mask"></div>
+    <form action="./../Controller/loginController.php" method="post">
+        
+
+    </form>
+</body>
+
+</html>
---
+++ SESAMO/database.php
@@ -0,0 +1,26 @@
+<?
+    class database{
+        private const DSN = 'mysql:dbname=reforcoEscolar;port=3306;host=localhost';
+        private const DUSER = 'root';
+        private const DPASSWORD = '';
+        // private const DSN = 'pgsql:dbname=reforcoEscolar;host=localhost';
+        // private const DUSER = 'postgres';
+        // private const DPASSWORD = 'postgres';
+        private static $conection = null;
+
+        static function conect(){
+        
+            try {
+                database::$conection = new PDO(database::DSN, database::DUSER, database::DPASSWORD);
+    
+                database::$conection->setAttribute(
+                    PDO::ATTR_ERRMODE, 
+                    PDO::ERRMODE_EXCEPTION
+                );
+    
+                return database::$conection;
+            } catch (PDOException $e) {
+                echo 'Conexão falhou: ' . $e->getMessage();
+            }
+        }
+    }
